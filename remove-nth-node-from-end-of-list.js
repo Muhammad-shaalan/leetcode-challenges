19dfcac99b6c562ff54bf696020ec1c089abbbe7
muhammad-shaalan <muhammadshaalan1422@gmail.com>
2022-11-26 21:30:22 +0200

    Add 'Remove Nth Node From End of List'

---
+++ remove-nth-node-from-end-of-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(-1, head);
+    let fast = dummy;
+    let slow = dummy;
+    for(let i = 1; i <= n + 1; i++) {
+        fast = fast.next;
+    }
+    while(fast != null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return dummy.next;
+};
